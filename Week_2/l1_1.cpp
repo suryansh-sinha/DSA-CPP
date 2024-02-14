ed555a36058d2b946ee9e537d5161bb038790596
Suryansh Sinha <suryanshsinha01@gmail.com>
2024-02-15 01:50:38 +0530

    add Week 2 - Lecture 1

--- Week_2/l1_1.cpp
+++ Week_2/l1_1.cpp
@@ -109,6 +109,70 @@ int main(){
         }
         cout << endl;
     }
+    cout << endl << endl;
+
+    // Diamond Using Spaces.
+    N=4;
+    for(int row=0; row<N; row++){
+        // Left star pattern.
+        for(int col=0; col<N-row; col++)
+            cout << "* ";
+        // Pyramid of Spaces
+        for(int col=0; col<2*row+1; col++)
+            cout << "  ";
+        // Last Pyramid
+        for(int col=0; col<N-row; col++)
+            cout << "* ";
+        cout << endl;
+    }
+    for(int row=0; row<N; row++){
+        // Left star pattern.
+        for(int col=0; col<row+1; col++)
+            cout << "* ";
+        // Pyramid of Spaces
+        for(int col=0; col<2*(N-row)-1; col++)
+            cout << "  ";
+        // Last Pyramid
+        for(int col=0; col<row+1; col++)
+            cout << "* ";
+        cout << endl;
+    }
+    cout << endl << endl;
+
+    // Star - Number Pattern
+    N = 5;
+    for(int row=0; row<N; row++){
+        for(int col=0; col<2*row+1; col++){
+            if(col%2 == 0)
+                cout << row+1 << " ";
+            else
+                cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int row=0; row<N-1; row++){
+        for(int col=0; col<2*N-2*row-3; col++){
+            if(col%2 == 0)
+                cout << N-row-1 << " ";
+            else
+                cout << "* ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+
+    // Hollow Inverted Half Pyramid
+    N = 6;
+    for(int row=0; row<N; row++){
+        for(int col=0; col<N-row; col++){
+            if (row==0 || col==0 || col==N-row-1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
 
     return 0;
 }
